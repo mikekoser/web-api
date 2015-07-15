bc0dc88cfad6c4456f09dd9460f1664a6e689d90
mikekoser <mikekoser@gmail.com>
2015-07-14 20:19:20 -0400

    Initial check-in

---
+++ Global.asax.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+using WebAPISandbox.App_Start;
+
+namespace WebAPISandbox
+{
+	public class Global : System.Web.HttpApplication
+	{
+		protected void Application_Start()
+		{
+			GlobalConfiguration.Configure(WebApiConfig.Register);
+		}
+	}
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,18 @@
+﻿
+namespace WebAPISandbox.Models
+{
+	public class Contact
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string EmailAddress { get; set; }
+		public string Phone { get; set; }
+		public string Street1 { get; set; }
+		public string Street2 { get; set; }
+		public string City { get; set; }
+		public string State { get; set; }
+		public string PostalCode { get; set; }
+		public string Country { get; set; }
+	}
+}
